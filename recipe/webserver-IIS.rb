3916f49d8563f0003791bd11cc59d4d203629238
arnab bhattacharya <myfriendarnab@gmail.com>
2017-12-09 00:24:09 +0530

    add a webpage to it

--- recipe/webserver-IIS.rb
+++ recipe/webserver-IIS.rb
@@ -10,3 +10,15 @@ powershell_script 'install-IIS' do
   guard_interpreter :powershell_script
   not_if "(Get-WindowsOptionalFeature -Online -FeatureName IIS-WebServer).State -eq 'Enabled'"
 end
+
+service 'w3svc' do
+  action [:enable, :start]
+end
+
+file 'c:\inetpub\wwwroot\Default.htm' do
+  content '<html>
+  <body>
+    <h1>hello world</h1>
+  </body>
+</html>'
+end
