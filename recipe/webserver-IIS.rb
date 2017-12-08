7060343829f55537e05a686ace2dcc1d6dbb32eb
arnab bhattacharya <myfriendarnab@gmail.com>
2017-12-09 00:17:19 +0530

    build fixes

--- recipe/webserver-IIS.rb
+++ recipe/webserver-IIS.rb
@@ -1,6 +1,12 @@
+# https://weblog.west-wind.com/posts/2017/May/25/Automating-IIS-Feature-Installation-with-Powershell
+powershell_script 'install-WebRole' do
+  code 'Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole'
+  guard_interpreter :powershell_script
+  not_if "(Get-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole).State -eq 'Enabled'"
+end
+
 powershell_script 'install-IIS' do
   code 'Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServer'
   guard_interpreter :powershell_script
   not_if "(Get-WindowsOptionalFeature -Online -FeatureName IIS-WebServer).State -eq 'Enabled'"
-  # https://weblog.west-wind.com/posts/2017/May/25/Automating-IIS-Feature-Installation-with-Powershell =end
 end
